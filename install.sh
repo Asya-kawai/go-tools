72a15a34ef3ab089b6d1f773b552d1cb561b34d8
Asya-kawai <kawai-toshiki@aintek.xyz>
2021-02-18 11:44:35 +0900

    Deleted gore-mode(go repl mode) because gore didn't match my usecase.

--- install.sh
+++ install.sh
@@ -32,9 +32,6 @@ go get -u github.com/cweill/gotests/...
 #go install google.golang.org/protobuf/cmd/protoc-gen-go
 go get -u github.com/golang/protobuf/protoc-gen-go
 
-# For go repl(gore).
-go get -u github.com/motemen/gore/cmd/gore
-
 # For debug.
 # Reference: https://github.com/go-delve/delve/blob/master/Documentation/installation/linux/install.md
 # Note: Can't execute 'go get ...' in module directory.
